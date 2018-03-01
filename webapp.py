57d560cefbd0ee9e2c41886e8b110116fde57ff9
kebudke <31251507+kebudke@users.noreply.github.com>
2018-03-01 08:50:47 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -58,7 +58,7 @@ def home():
 
 @app.route('/posted', methods=['POST'])
 def post():
-    update_data([session['user_data']['login'], request.form['post']])
+    update_data(Markup([session['user_data']['login'], request.form['post']]))
     render_template('home.html', past_posts=posts_to_html())
 
 #redirect to GitHub's OAuth page and confirm callback URL

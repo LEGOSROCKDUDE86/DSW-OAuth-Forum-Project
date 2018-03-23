6e850e4cb2891856bf1e5b932eefe21be62f42f8
kebudke <31251507+kebudke@users.noreply.github.com>
2018-03-22 18:48:47 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -56,15 +56,15 @@ def update_data(post):
 #     return ret
 
 def posts_to_html():
+    post_table = Markup("<table class='table table-bordered'><tr><th>User</th><th>Post</th></tr>")
     try:
         with open('posts.json', 'r') as forumPosts:
-            post_table = Markup("<table class='table table-bordered'><tr><th>User</th><th>Post</th></tr>")
             posts = json.load(forumPosts)
             for p in posts:
-                post_table += Markup("<tr><td>" + p['username'] + "</td><td>" + p["message"] + "</td></tr>")
+                        print(p)
+                        post_table += Markup("<tr><td>" + p['username'] + "</td><td>" + p["message"] + "</td></tr>")
             post_table += Markup("</table>")
     except Exception as e:
-        post_table = ''
         print(e)
     return post_table
             

633440f935815e3bda57013463bd5ff3557680ed
kebudke <31251507+kebudke@users.noreply.github.com>
2018-06-08 00:58:34 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -52,7 +52,7 @@ def home():
 @app.route('/delete', methods=['POST'])
 def delete():
     id = ObjectId(request.form['delete'])
-    db.posts.delete_one({'_id':id}))
+    db.posts.delete_one({'_id':id})
     return render_template('home.html', past_posts=posts_to_html())
 	
 def posts_to_html():
